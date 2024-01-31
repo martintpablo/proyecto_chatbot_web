859e6ec18201a9dc81a420409a421916a8f644be
Alberto <albertomorenogonzalez03@gmail.com>
2024-01-31 19:31:36 +0100

    Models, Core Module and Chart.js Test

---
+++ src/app/core/services/message.model.ts
@@ -0,0 +1,6 @@
+export interface Message {
+    id:string,
+    content:string,
+    datetime:string,
+    userId:string
+  }
---
+++ src/app/core/services/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id:string,
+    prof:boolean,
+    name:string,
+    surname:string,
+    birthdate:string,
+    email:string,
+    gender:string,
+    profilePick?:string
+    class?:string
+  }
