49ec6e0985b3e971a01524ddea8378f037490378
PabloMartinTrujillo <pablo.martin.alu@iescampanillas.com>
2024-02-02 17:52:19 +0100

    added changes for login component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    LoginComponent
+  ],
+  providers: [],
+})
+export class CoreModule { }
