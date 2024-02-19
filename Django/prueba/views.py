845bf21f0b090b3bf6b01ee3385cde315c25539e
Elena Racero González <146923466+ElenaRacero3@users.noreply.github.com>
2024-02-19 22:48:27 +0100

    Subida Django

---
+++ Django/prueba/views.py
@@ -0,0 +1,14 @@
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+
+@csrf_exempt
+def chatbot_view(request):
+    if request.method == 'GET':
+        response_data = {"message": "Hola. Has realizado una solicitud GET a la página de inicio."}
+        return JsonResponse(response_data)
+    elif request.method == 'POST':
+        response_data = {"message": "Hola. Has realizado una solicitud POST a la página de inicio."}
+        return JsonResponse(response_data)
+    else:
+        return HttpResponse(status=405) 
